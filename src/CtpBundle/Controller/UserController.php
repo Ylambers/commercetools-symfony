d5964fe95ab97aca044bb8b5d89e1a7c58c124b7
Yaron Lambers <Firstname SurnamePosition at CTE. mail@commercetools.deT. +49 (30) 467 24 21-20M. +49 0000 00000000commercetools GmbHSchumannstr. 17yaron.lambers@commercetools.de>
2016-03-10 16:38:52 +0100

    login with user

--- src/CtpBundle/Controller/UserController.php
+++ src/CtpBundle/Controller/UserController.php
@@ -6,17 +6,34 @@
 namespace Commercetools\Symfony\CtpBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class UserController extends Controller
 {
-    public function loginAction()
+    public function indexAction()
     {
-//        return $this->render('CtpBundle:user:login.html.twig', array(
-//            'foo' => 'Return new repsone of foo / UserController'
-//        ));
+        return new Response('Test');
+    }
 
-        return new Response('<html><body>Admin Page</body></html>');
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('CtpBundle:user:login.html.twig',
+            array(
+                'last_username' => $lastUsername,
+                'error' => $error
+            ));
+    }
+
+    public function loginCheckAction()
+    {
+        
     }
 }
