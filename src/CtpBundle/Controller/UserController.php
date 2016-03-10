1f48a38547e18d70a973f33aba2414df9430ab4d
Yaron Lambers <Firstname SurnamePosition at CTE. mail@commercetools.deT. +49 (30) 467 24 21-20M. +49 0000 00000000commercetools GmbHSchumannstr. 17yaron.lambers@commercetools.de>
2016-03-10 13:57:27 +0100

    Login  option

--- src/CtpBundle/Controller/UserController.php
+++ src/CtpBundle/Controller/UserController.php
@@ -6,13 +6,17 @@
 namespace Commercetools\Symfony\CtpBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class UserController extends Controller
 {
     public function loginAction()
     {
-        return $this->render('CtpBundle:user:login.html.twig', array(
-            'foo' => 'Return new repsone of foo'
-        ));
+//        return $this->render('CtpBundle:user:login.html.twig', array(
+//            'foo' => 'Return new repsone of foo / UserController'
+//        ));
+
+        return new Response('<html><body>Admin Page</body></html>');
     }
 }
